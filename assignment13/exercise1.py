df1a1f57bee90006f2d4def167177a7b80c30cd9
Sonira Shokohyan <138888725+Sonirashokohyan@users.noreply.github.com>
2023-07-30 11:46:42 +0500

    Delete assignment13 directory

--- assignment13/exercise1.py
+++
@@ -1,8 +0,0 @@
-def print_message(message, repeat_count = 1):
-    s = 1
-    while s <= repeat_count:
-        print(message)
-        s += 1
-print_message("This is Sonira Shokohyan business center", 5)
-print_message("Sonira shokohyan",)
-    
